061885e9fd50bbcf67dc0031153a1721f8a733bd
DanieleG27 <daniguidu02@gmail.com>
2025-03-06 17:03:39 +0100

    Lezione 06/03/25

---
+++ ProjectClient/src/test/java/unibo/disi/ProjectClient/ProjectClientApplicationTests.java
@@ -0,0 +1,13 @@
+package unibo.disi.ProjectClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
